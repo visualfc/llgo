c28eb46f712c1639eede3a69153d35cc51d0bed3
visualfc <visualfc@gmail.com>
2025-08-05 21:43:47 +0800

    x2

--- internal/cabi/arch.go
+++ internal/cabi/arch.go
@@ -361,7 +361,7 @@ func (p *TypeInfoArm) GetTypeInfo(ctx llvm.Context, typ llvm.Type, bret bool) *T
 			} else {
 				info.Kind = AttrWidthType
 				if hasTypes(types, ctx.DoubleType()) {
-					info.Type1 = llvm.ArrayType(ctx.Int64Type(), (info.Size/8+3)&^3)
+					info.Type1 = llvm.ArrayType(ctx.Int64Type(), (info.Size/8+7)&^7)
 				} else {
 					info.Type1 = llvm.ArrayType(ctx.Int32Type(), (info.Size/4+3)&^3)
 				}
