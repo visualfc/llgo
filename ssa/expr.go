e8f2ff1b514935af7ccf74b232c87860b3e3cc7e
visualfc <visualfc@gmail.com>
2026-06-11 17:04:01 +0800

    ssa: fix reflect.Value.Slice for pkg.NeedAbiInit

--- ssa/expr.go
+++ ssa/expr.go
@@ -1243,7 +1243,7 @@ func (b Builder) checkReflect(fn Expr, args []Expr) {
 		pkg.NeedAbiInit |= ReflectMapOf
 	case "reflect.PointerTo", "reflect.PtrTo":
 		pkg.NeedAbiInit |= ReflectPointerTo
-	case "reflect.SliceOf":
+	case "reflect.SliceOf", "reflect.Value.Slice":
 		pkg.NeedAbiInit |= ReflectSliceOf
 	case "reflect.StructOf":
 		pkg.NeedAbiInit |= ReflectStructOf
