150c757ab2532ea4583897df3b19ccdaed18a410
Li Jie <cpunion@gmail.com>
2026-07-29 21:23:34 +0800

    demo(thread): keep pthread key example

--- _demo/c/thread/thd.go
+++ _demo/c/thread/thd.go
@@ -5,21 +5,21 @@ import (
 	"github.com/goplus/lib/c/pthread"
 )
 
-//llgo:tls
-var threadValue uintptr
+var key pthread.Key
 
 func main() {
-	threadValue = uintptr(c.Pointer(c.Str("main value\n")))
+	key.Create(nil)
+	key.Set(c.Pointer(c.Str("main value\n")))
 
 	var thd pthread.Thread
 	pthread.Create(&thd, nil, func(arg c.Pointer) c.Pointer {
-		threadValue = uintptr(c.Pointer(c.Str("thread value\n")))
-		c.Printf(c.Str("Hello, thread\nTLS: %s"), c.Pointer(threadValue))
+		key.Set(c.Pointer(c.Str("thread value\n")))
+		c.Printf(c.Str("Hello, thread\nTLS: %s"), key.Get())
 		return c.Pointer(c.Str("Back to main\n"))
 	}, nil)
 
 	var retval c.Pointer
 	pthread.Join(thd, &retval)
 
-	c.Printf(c.Str("%sTLS: %s"), retval, c.Pointer(threadValue))
+	c.Printf(c.Str("%sTLS: %s"), retval, key.Get())
 }
