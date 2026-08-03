2353dec74d1f59536f29e3e1f0dc17d1d15ba197
Li Jie <cpunion@gmail.com>
2026-08-03 19:31:11 +0800

    test: cover defer selector dispatch end to end

--- internal/build/testdata/wasm-runtime/main.go
+++ internal/build/testdata/wasm-runtime/main.go
@@ -3,5 +3,40 @@ package main
 import "runtime"
 
 func main() {
+	exerciseDeferContinuations()
 	println(runtime.GOOS)
+}
+
+func exerciseDeferContinuations() {
+	if got := normalLoopDefers(); got != 321 {
+		panic("normal loop defer order")
+	}
+	order, recovered := panicWhileDrainingLoopDefers()
+	if order != 321 || recovered != "wasm-loop-defer-boom" {
+		panic("panic loop defer order")
+	}
+}
+
+func normalLoopDefers() (order int) {
+	for i := 1; i <= 3; i++ {
+		value := i
+		defer func() { order = order*10 + value }()
+	}
+	return
+}
+
+func panicWhileDrainingLoopDefers() (order int, recovered any) {
+	defer func() { recovered = recover() }()
+	func() {
+		for i := 1; i <= 3; i++ {
+			value := i
+			defer func() {
+				order = order*10 + value
+				if value == 2 {
+					panic("wasm-loop-defer-boom")
+				}
+			}()
+		}
+	}()
+	return
 }
