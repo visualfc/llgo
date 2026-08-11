3981e99c3c883df9eb5728971ddf6bf83884b9ca
Li Jie <cpunion@gmail.com>
2026-08-11 15:02:26 +0800

    runtime: replace superseded panics across defer frames

--- runtime/internal/runtime/z_default.go
+++ runtime/internal/runtime/z_default.go
@@ -22,8 +22,9 @@ func Rethrow(link *Defer) {
 		if gp.panicIsSuspended(ptr) {
 			return
 		}
+		node := (*panicNode)(ptr)
+		gp.movePanicToDefer(node, link)
 		if link == nil {
-			node := (*panicNode)(ptr)
 			TracePanic(node.arg)
 			if PanicTraceback == nil || !PanicTraceback(2) {
 				debug.PrintStack(2)
@@ -31,10 +32,6 @@ func Rethrow(link *Defer) {
 			c.Free(unsafe.Pointer(node))
 			c.Exit(2)
 		} else {
-			node := (*panicNode)(ptr)
-			if link == gp.defer_ {
-				node.moveToDefer(link)
-			}
 			c.Siglongjmp(link.Addr, 1)
 		}
 	} else if gp.goexit {
@@ -43,6 +40,7 @@ func Rethrow(link *Defer) {
 		// 1) If we have a defer frame, longjmp to it so it can execute defers.
 		// 2) Once we've unwound past the last frame (link==nil), terminate the
 		//    current pthread.
+		gp.defer_ = link
 		if link != nil {
 			c.Siglongjmp(link.Addr, 1)
 		}
