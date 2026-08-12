bf706b2d5a39c5c98cf222f036cd241741fc5c4a
visualfc <visualfc@gmail.com>
2026-08-12 12:18:13 +0800

    runtime/internal/runtime/goarch: fix endian

--- runtime/internal/clite/goarch/endian_little.go
+++ runtime/internal/clite/goarch/endian_little.go
@@ -1,5 +1,5 @@
-//go:build ppc64 || s390x || mips || mips64
-// +build ppc64 s390x mips mips64
+//go:build 386 || amd64 || arm || arm64 || loong64 || mips64le || mipsle || ppc64le || riscv64 || wasm
+// +build 386 amd64 arm arm64 loong64 mips64le mipsle ppc64le riscv64 wasm
 
 package goarch
 
---
+++ runtime/internal/runtime/goarch/endian_test.go
@@ -0,0 +1,19 @@
+package goarch_test
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/goplus/llgo/runtime/internal/runtime/goarch"
+)
+
+func TestEndianConstants(t *testing.T) {
+	v := uint16(0x0102)
+	actualLittleEndian := *(*byte)(unsafe.Pointer(&v)) == 0x02
+	if goarch.LittleEndian != actualLittleEndian {
+		t.Fatalf("LittleEndian = %v, actual memory layout is little-endian: %v", goarch.LittleEndian, actualLittleEndian)
+	}
+	if goarch.BigEndian == goarch.LittleEndian {
+		t.Fatalf("BigEndian = %v, LittleEndian = %v; want opposite values", goarch.BigEndian, goarch.LittleEndian)
+	}
+}
