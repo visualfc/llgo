feff6ee36f2e2abf9102fdd22531812bfbefd95a
Li Jie <cpunion@gmail.com>
2026-08-21 08:46:06 +0800

    runtime: make Windows process exit race-safe

--- test/windows/process_test.go
+++ test/windows/process_test.go
@@ -3,9 +3,14 @@
 package windowstesting
 
 import (
+	"context"
+	"os"
 	"os/exec"
 	"testing"
+	"time"
 )
+
+const exitDuringThreadCreateHelper = "LLGO_WINDOWS_EXIT_THREAD_CREATE_HELPER"
 
 func TestRepeatedProcessLifecycle(t *testing.T) {
 	const iterations = 32
@@ -14,5 +19,61 @@ func TestRepeatedProcessLifecycle(t *testing.T) {
 		if err := cmd.Run(); err != nil {
 			t.Fatalf("iteration %d: %v", i, err)
 		}
+	}
+}
+
+func TestExitDuringThreadCreation(t *testing.T) {
+	if mode := os.Getenv(exitDuringThreadCreateHelper); mode != "" {
+		runExitThreadSieve()
+		if mode == "os-exit" {
+			os.Exit(0)
+		}
+		return
+	}
+
+	const iterations = 64
+	for _, mode := range []string{"return", "os-exit"} {
+		for i := 0; i < iterations; i++ {
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			cmd := exec.CommandContext(ctx, os.Args[0], "-test.run=^TestExitDuringThreadCreation$")
+			cmd.Env = append(os.Environ(), exitDuringThreadCreateHelper+"="+mode)
+			output, err := cmd.CombinedOutput()
+			timedOut := ctx.Err() != nil
+			cancel()
+			if timedOut {
+				t.Fatalf("%s iteration %d: child process did not exit\n%s", mode, i, output)
+			}
+			if err != nil {
+				t.Fatalf("%s iteration %d: %v\n%s", mode, i, err, output)
+			}
+		}
+	}
+}
+
+func runExitThreadSieve() {
+	primes := make(chan int)
+	go func() {
+		ch := make(chan int)
+		go func() {
+			for value := 2; ; value++ {
+				ch <- value
+			}
+		}()
+		for {
+			prime := <-ch
+			primes <- prime
+			next := make(chan int)
+			go func(input <-chan int, output chan<- int, divisor int) {
+				for value := range input {
+					if value%divisor != 0 {
+						output <- value
+					}
+				}
+			}(ch, next, prime)
+			ch = next
+		}
+	}()
+	for range 25 {
+		<-primes
 	}
 }
