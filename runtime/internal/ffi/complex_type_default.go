78de22ef1911b0bfd1548dab974734bf7df26054
Li Jie <cpunion@gmail.com>
2026-08-25 00:10:21 +0800

    ffi: use the current runtime module path

--- runtime/internal/ffi/complex_type_default.go
+++ runtime/internal/ffi/complex_type_default.go
@@ -2,7 +2,7 @@
 
 package ffi
 
-import "github.com/goplus/llgo/runtime/internal/clite/ffi"
+import "github.com/xgo-dev/llgo/runtime/internal/clite/ffi"
 
 func newComplexType(elem *Type, size uintptr, align uint16) *Type {
 	return &Type{size, align, ffi.Complex, &[]*Type{elem, nil}[0]}
