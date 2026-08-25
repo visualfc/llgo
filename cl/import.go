190616bacf12d6557533be10289215c0db689fc6
Li Jie <cpunion@gmail.com>
2026-08-25 08:46:56 +0800

    cl: remove unreachable setjmp intrinsics

--- cl/import.go
+++ cl/import.go
@@ -604,10 +604,7 @@ const (
 	llgoSigjmpbuf  = llgoInstrBase + 0xb
 	llgoSigsetjmp  = llgoInstrBase + 0xc
 	llgoSiglongjmp = llgoInstrBase + 0xd
-	llgoSetjmp     = llgoInstrBase + 0xf
-	llgoLongjmp    = llgoInstrBase + 0x13
-
-	llgoFuncAddr = llgoInstrBase + 0xe
+	llgoFuncAddr   = llgoInstrBase + 0xe
 
 	llgoPyList  = llgoInstrBase + 0x10
 	llgoPyStr   = llgoInstrBase + 0x11
