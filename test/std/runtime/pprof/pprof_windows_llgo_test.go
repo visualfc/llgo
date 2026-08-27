4973ccf1059fea0e346c4c0cde3d63e7b63b60aa
Li Jie <cpunion@gmail.com>
2026-08-27 19:29:29 +0800

    test: synchronize Windows CPU profile sampling

--- test/std/runtime/pprof/pprof_windows_llgo_test.go
+++ test/std/runtime/pprof/pprof_windows_llgo_test.go
@@ -4,11 +4,34 @@ package pprof_test
 
 import (
 	"testing"
+	"time"
 	_ "unsafe"
 )
 
 //go:linkname testCPUProfileWindowsFaultRecovery C.llgo_cpu_profile_test_fault_recovery
 func testCPUProfileWindowsFaultRecovery() int32
+
+//go:linkname currentThreadCPUProfileSamples C.llgo_cpu_profile_test_current_thread_samples
+func currentThreadCPUProfileSamples() uint64
+
+//go:linkname clearCPUProfileTestThread C.llgo_cpu_profile_test_clear_thread
+func clearCPUProfileTestThread()
+
+func init() {
+	waitForCPUProfileSample = func() {
+		before := currentThreadCPUProfileSamples()
+		deadline := time.Now().Add(time.Second)
+		// Keep this thread on-CPU until the sampler captures it. Sleeping or
+		// yielding here could deschedule the thread and defeat the barrier.
+		for currentThreadCPUProfileSamples() == before {
+			if time.Now().After(deadline) {
+				clearCPUProfileTestThread()
+				panic("Windows CPU profiler did not sample the current thread within 1s")
+			}
+		}
+		clearCPUProfileTestThread()
+	}
+}
 
 func TestCPUProfileWindowsFaultRecovery(t *testing.T) {
 	if got := testCPUProfileWindowsFaultRecovery(); got != 1 {
