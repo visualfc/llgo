b8a5ee1194404de8ba4f72514bb8fff5a2f98e6b
Li Jie <cpunion@gmail.com>
2026-09-09 16:16:18 +0800

    test/goroot: update two-release expectations

--- test/goroot/runner_unit_test.go
+++ test/goroot/runner_unit_test.go
@@ -347,16 +347,50 @@ func TestObservedFailuresHaveXFailClassifications(t *testing.T) {
 		platform string
 		tc       testCase
 	}{
-		{version: "go1.27.0", platform: "darwin/arm64", tc: testCase{RelPath: "index0.go", Directive: "runoutput"}},
+		{version: "go1.27.0", platform: "windows-mingw/386", tc: testCase{RelPath: "index0.go", Directive: "runoutput"}},
 		{version: "go1.27.0", platform: "linux/amd64", tc: testCase{RelPath: "rangegen.go", Directive: "runoutput"}},
 		{version: "go1.27.0", platform: "linux/amd64", tc: testCase{RelPath: "fixedbugs/issue34123.go", Directive: "run"}},
-		{version: "go1.27.0", platform: "darwin/arm64", tc: testCase{RelPath: "fixedbugs/issue52612.go", Directive: "run"}},
 		{version: "go1.27.0", platform: "linux/amd64", tc: testCase{RelPath: "heapsampling.go", Directive: "run"}},
 		{version: "go1.26.7", platform: "linux/amd64", tc: testCase{RelPath: "convert5.go", Directive: "run"}},
+		{version: "go1.26.7", platform: "windows-msvc/amd64", tc: testCase{RelPath: "linkmain_run.go", Directive: "run"}},
 	}
 	for _, tt := range tests {
 		if match, _ := cfg.Match(tt.version, tt.platform, tt.tc); !match {
 			t.Errorf("%s did not match xfail for %s/%s", tt.tc.RelPath, tt.version, tt.platform)
+		}
+	}
+}
+
+func TestObservedPassesDoNotHaveXFailClassifications(t *testing.T) {
+	repo := repoRoot(t)
+	cfg := loadXFailConfig(t, repo, filepath.Join("test", "goroot", "xfail.yaml"))
+	tests := []struct {
+		version  string
+		platform string
+		tc       testCase
+	}{
+		{version: "go1.27.0", platform: "darwin/arm64", tc: testCase{RelPath: "index0.go", Directive: "runoutput"}},
+		{version: "go1.27.0", platform: "windows-msvc/386", tc: testCase{RelPath: "rangegen.go", Directive: "runoutput"}},
+		{version: "go1.27.0", platform: "windows-msvc/amd64", tc: testCase{RelPath: "fixedbugs/issue34123.go", Directive: "run"}},
+		{version: "go1.27.0", platform: "darwin/arm64", tc: testCase{RelPath: "fixedbugs/issue52612.go", Directive: "run"}},
+	}
+	for _, tt := range tests {
+		if match, reason := cfg.Match(tt.version, tt.platform, tt.tc); match {
+			t.Errorf("%s unexpectedly matched xfail for %s/%s: %s", tt.tc.RelPath, tt.version, tt.platform, reason)
+		}
+	}
+}
+
+func TestWindows386Issue25897aIsFlakyWithTimeout(t *testing.T) {
+	repo := repoRoot(t)
+	cfg := loadXFailConfig(t, repo, filepath.Join("test", "goroot", "xfail.yaml"))
+	tc := testCase{RelPath: "fixedbugs/issue25897a.go", Directive: "run"}
+	for _, platform := range []string{"windows-msvc/386", "windows-mingw/386"} {
+		if match, _ := cfg.MatchFlaky("go1.26.7", platform, tc); !match {
+			t.Errorf("%s did not match flake for %s", tc.RelPath, platform)
+		}
+		if timeout, _, match := cfg.MatchTimeout("go1.26.7", platform, tc); !match || timeout != 90*time.Second {
+			t.Errorf("timeout for %s/%s = %s, %v; want 1m30s, true", tc.RelPath, platform, timeout, match)
 		}
 	}
 }
